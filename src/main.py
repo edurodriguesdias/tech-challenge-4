578a5f69802e3740b9b6fb08d94b99ed9541282f
Felipe Langoni Ramos <felipelangoniramos@pop-os.localdomain>
2024-11-14 23:51:26 -0300

    feat: API and traning file

--- src/main.py
+++ src/main.py
@@ -1,77 +1,39 @@
-from fastapi import FastAPI
-import requests
-import datetime
-import pandas as pd
+from tensorflow.keras.models import load_model
+import joblib
 import json
-from .sanitize import Sanitize
-from .database import engine
-from sklearn.cluster import KMeans
-from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt
-import os
+import requests
+import numpy as np
+from fastapi import FastAPI
+import pickle
+from pydantic import BaseModel
+
+class PredictionInput(BaseModel):
+    ticker: str
+    look_back: int
+    dias_para_prever: int
+
+def predict_future_price(ticker, look_back, dias_para_prever):
+
+    modelo = load_model(f'data/{ticker}_modelo.h5')
+    scaler = joblib.load(f'data/{ticker}_scaler.pkl')
+
+    predictions = []
+    current_input = scaled_data[-look_back:].reshape(1, look_back, 1)
+    
+    for _ in range(dias_para_prever):
+        next_day_prediction = modelo.predict(current_input)
+        next_day_prediction = scaler.inverse_transform(next_day_prediction)
+        predictions.append(next_day_prediction[0][0])
+        
+        current_input = np.append(current_input[:, 1:, :], next_day_prediction.reshape(1, 1, 1), axis=1)
+    predictions = [int(value) for value in predictions]
+
+    return {"predictions": predictions}
 
 app = FastAPI()
+with open("data/scaled_data.pkl", 'rb') as file:
+    scaled_data = pickle.load(file)
 
-@app.get("/extract-data")
-def download_and_extract():
-    current_date = datetime.datetime.now().strftime("%Y-%m-%d")
-
-    url = "https://sistemaswebb3-listados.b3.com.br/indexProxy/indexCall/GetPortfolioDay/eyJsYW5ndWFnZSI6InB0LWJyIiwicGFnZU51bWJlciI6MSwicGFnZVNpemUiOjEyMCwiaW5kZXgiOiJJQk9WIiwic2VnbWVudCI6IjIifQ=="
-
-    response = requests.get(url, verify=False)
-
-    if response.ok:
-        response = json.loads(response.content.decode()).get("results")
-        df = pd.DataFrame(response)
-
-        df = Sanitize.clean_df(df)
-        df["data_pregao"] = current_date
-        df.to_sql("b3_data",engine,index=False, if_exists="replace")
-
-        return {"message": "Dados coletados e armazenados com sucesso!"}
-    else:
-        return {"error": "Erro ao coletar os dados"}
-
-@app.get("/training-model")
-def trainingModel():
-    df = pd.read_sql("b3_data", engine)
-    df_scaled = __get_scaled_data(df)
-    
-    kmeans = KMeans(n_clusters=2, random_state=0)
-    kmeans.fit(df_scaled)
-    df['Cluster'] = kmeans.fit_predict(df_scaled)
-
-    return df.to_json(orient='records')
-
-@app.get("/optimal-centroid-number")
-def best_value_for_k_elbow_method():
-    WSS = []
-    K = range(1, 10)
-
-    df = pd.read_sql("b3_data", engine)
-    scaled_data = __get_scaled_data(df)
-    
-    for k in K:
-        kmeansInstance = KMeans(n_clusters=k)
-        kmeansInstance.fit(scaled_data)
-        WSS.append(kmeansInstance.inertia_)
-
-    plt.figure(figsize=(10,5))
-    plt.plot(K, WSS, '-bo')
-    plt.xlabel('k')
-    plt.ylabel('WSS')
-    plt.title('Elbow Method - Optimal Centroid Number')
-    
-    images_dir = 'images'
-    if not os.path.exists(images_dir):
-        os.makedirs(images_dir)
-    
-    file_path = os.path.join(images_dir, 'elbow_method.png')
-
-    plt.savefig(file_path, format='png')
-
-    return {"message": f"Gráfico salvo em: {file_path}"}
- 
-def __get_scaled_data(df):
-    scaler = StandardScaler()
-    return scaler.fit_transform(df[['quantidade_teorica', 'porcentagem_participacao']])
+@app.post("/predict")
+def predict(data:PredictionInput):
+    return predict_future_price(data.ticker, data.look_back, data.dias_para_prever)
